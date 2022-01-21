c940b546f14cf8ed1e6c038c34fa0ca4e3b5e192
Thore Goll <thore.goll@franka.de>
2022-01-21 13:49:37 +0100

    CHANGE: Tab -> Spaces

--- setup.sh
+++ setup.sh
@@ -28,15 +28,15 @@ sudo fc-cache -f -v   # Refresh the font cache
 
 if [ -d "ccls" ]; then
   echo "Now building CCLS C/C++ Language Server"
-	cd ccls || return
-	if [ -d llvm ]; then rm -Rf llvm; fi
-	mkdir -p llvm && cd llvm || return
-	LLVM_BASENAME="clang+llvm-9.0.0-$(uname -m)-linux-gnu-ubuntu-$(lsb_release -rs)"
-	wget "https://releases.llvm.org/9.0.0/${LLVM_BASENAME}.tar.xz"
-	tar -xf "${LLVM_BASENAME}.tar.xz"
-	cd ..
-	cmake -H. -BRelease -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH="./llvm/${LLVM_BASENAME}"
-	cmake --build Release -- "-j$(nproc)"
+  cd ccls || return
+  if [ -d llvm ]; then rm -Rf llvm; fi
+  mkdir -p llvm && cd llvm || return
+  LLVM_BASENAME="clang+llvm-9.0.0-$(uname -m)-linux-gnu-ubuntu-$(lsb_release -rs)"
+  wget "https://releases.llvm.org/9.0.0/${LLVM_BASENAME}.tar.xz"
+  tar -xf "${LLVM_BASENAME}.tar.xz"
+  cd ..
+  cmake -H. -BRelease -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH="./llvm/${LLVM_BASENAME}"
+  cmake --build Release -- "-j$(nproc)"
 
 else
     echo "No ccls folder found. Was the submodule cloned successfully? Skipping CCLS installation"
